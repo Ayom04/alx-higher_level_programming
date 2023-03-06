efbd5d05041244b9db3db9d4c77c5f0d735c7ac8
Ayom04 <abdullahayomide04@gmail.com>
2023-03-06 12:15:07 +0100

    list

---
+++ 0x00-python-hello_world/hello.py
@@ -0,0 +1,16 @@
+# school = "holbertson"
+# print(school[-1: 4])
+# # H     o   l   b   e   r   t   s   o   n
+# # 0     1   2   3   4   5   6   7   8   9 
+# #-10   -9  -8   -7  -6  -5 -4   -3  -2  -1
+
+# first_name = "Eric"
+# last_name = "Idle"
+# age = 74
+# profession = "comedian"
+# affiliation = "Monty Python"
+# "Hello, %s %s. You are %s. You are a %s. You were a member of %s." % (first_name, last_name, age, profession, affiliation)
+# 'Hello, Eric Idle. You are 74. You are a comedian. You were a member of Monty Python.'
+#  print(f"{98} Battery street, {'San Francisco'}")
+a = "Python is cool"
+print(a[7:-5])
