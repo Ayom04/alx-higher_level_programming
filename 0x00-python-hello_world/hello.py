ae69f014bafdc9ff2a132be1868fd75d4facb565
Ayom04 <abdullahayomide04@gmail.com>
2023-03-06 18:41:01 +0100

    write

--- 0x00-python-hello_world/hello.py
+++ 0x00-python-hello_world/hello.py
@@ -1,16 +1,27 @@
-# school = "holbertson"
+#school = "holbertson"
 # print(school[-1: 4])
-# # H     o   l   b   e   r   t   s   o   n
-# # 0     1   2   3   4   5   6   7   8   9 
-# #-10   -9  -8   -7  -6  -5 -4   -3  -2  -1
+# H     o   l   b   e   r   t   s   o   n
+# 0     1   2   3   4   5   6   7   8   9 
+# -10   -9  -8   -7  -6  -5 -4   -3  -2  -1
 
-# first_name = "Eric"
-# last_name = "Idle"
-# age = 74
-# profession = "comedian"
-# affiliation = "Monty Python"
-# "Hello, %s %s. You are %s. You are a %s. You were a member of %s." % (first_name, last_name, age, profession, affiliation)
-# 'Hello, Eric Idle. You are 74. You are a comedian. You were a member of Monty Python.'
-#  print(f"{98} Battery street, {'San Francisco'}")
+first_name = "Eric"
+last_name = "Idle"
+age = 74
+profession = "comedian"
+affiliation = "Monty Python"
+"Hello, %s %s. You are %s. You are a %s. You were a member of %s." % (first_name, last_name, age, profession, affiliation)
+'Hello, Eric Idle. You are 74. You are a comedian. You were a member of Monty Python.'
+print(f"{98} Battery street, {'San Francisco'}")
+
 a = "Python is cool"
 print(a[7:-5])
+import sys
+
+
+def print_to_stderr(*a):
+
+	# Here a is the array holding the objects
+	# passed as the argument of the function
+	print(*a, file = sys.stderr)
+
+print_to_stderr("Hello World")
