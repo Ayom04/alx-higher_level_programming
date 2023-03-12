930636cb0c8d33f335b659d915266008be5c229a
Ayom04 <abdullahayomide04@gmail.com>
2023-03-12 20:43:34 +0100

    test

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,3 +5,10 @@ def multiple_returns(sentence):
     else:
         tuple_res = (len(sentence), sentence[0])
     return (tuple_res)
+
+
+multiple_returns = __import__('8-multiple_returns').multiple_returns
+
+sentence = "At school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
