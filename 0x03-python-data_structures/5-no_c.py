482b8420a56a247408c4964f47fa9047db23bed5
Ayom04 <abdullahayomide04@gmail.com>
2023-03-12 21:27:33 +0100

    test

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,3 +2,9 @@
 def no_c(my_string):
     new_string = my_string.translate({ord(i): None for i in 'cC'})
     return new_string
+
+no_c = __import__('5-no_c').no_c
+
+print(no_c("Best School"))
+print(no_c("Chicago"))
+print(no_c("C is fun!"))
